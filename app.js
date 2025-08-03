d0e83ce7d48ba200689233650fb037bdc4b54b12
AndenPMS <Philipp_Schuster@hotmail.de>
2025-08-03 04:01:41 -0300

    Progreso

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() { 
+    let input = document.getElementById('amigo'); 
+    amigos.push(input.value);
+}
+
+console.log(amigos)
